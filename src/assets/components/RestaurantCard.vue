1c937c09abb01d1737ce29095411c3f2da8ce83f
plastikSun <marcom2k01@gmail.com>
2023-11-28 18:16:51 +0100

    add: card html, fetch data from API endpoint (incomplete)

--- src/assets/components/RestaurantCard.vue
+++ src/assets/components/RestaurantCard.vue
@@ -1,7 +1,25 @@
 <script>
+import { store } from "../data/store";
+import axios from "axios";
+
 export default {
   data() {
-    return {};
+    return {
+      menu: [],
+    };
+  },
+
+  methods: {
+    fetchMenu(endpoint) {
+      axios.get(endpoint).then((response) => {
+        this.menu = response.data;
+        console.log(this.menu);
+      });
+    },
+  },
+
+  mounted() {
+    //this.fetchMenu(store.baseUri + "menu");
   },
 
   props: { element: Object },
@@ -9,21 +27,24 @@ export default {
 </script>
 
 <template>
-  <!-- {{ console.log(element) }} -->
-  <div class="card mb-3">
-    <img :src="element.image" class="card-img-top" />
-    <div class="card-body">
-      <h5 class="card-title">
-        {{ element.restaurant_name }}
-      </h5>
-      <p>{{ element.address }}</p>
-      <p class="card-text">
-        {{ element.description }}
-      </p>
-      <p v-for="typeEl in element.types" class="card-text">
-        <small class="text-body-secondary">{{ typeEl.name }}</small>
-      </p>
-    </div>
+  {{ console.log(element) }}
+
+  <div class="card mb-3" @click="loadRestaurant">
+    <router-link :to="{ name: 'restaurant', params: { id: element.id } }">
+      <img :src="element.image" class="card-img-top" />
+      <div class="card-body">
+        <h5 class="card-title" @click="loadRestaurant">
+          {{ element.restaurant_name }}
+        </h5>
+        <p>{{ element.address }}</p>
+        <p class="card-text">
+          {{ element.description }}
+        </p>
+        <p v-for="typeEl in element.types" class="card-text">
+          <small class="text-body-secondary">{{ typeEl.name }}</small>
+        </p>
+      </div>
+    </router-link>
   </div>
 </template>
 
