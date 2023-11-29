95d33b2ba4e5dfb76bea891eb4e05773df4f9187
plastikSun <marcom2k01@gmail.com>
2023-11-29 16:57:44 +0100

    add: navbar component

--- src/App.vue
+++ src/App.vue
@@ -1,12 +1,19 @@
 <script>
+import Navbar from "../src/assets/components/partials/Navbar.vue";
+
 export default {
   data() {
     return {};
+  },
+
+  components: {
+    Navbar,
   },
 };
 </script>
 
 <template>
+  <Navbar></Navbar>
   <div class="container mt-5">
     <router-view></router-view>
   </div>
