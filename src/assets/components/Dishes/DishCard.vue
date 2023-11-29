2c92089a33e35d72e0e44f51a5dd2b3ae8c42747
RodiaGiordano <rodiagiordano93@gmail.com>
2023-11-29 16:24:36 +0100

    fix after merge

--- src/assets/components/Dishes/DishCard.vue
+++ src/assets/components/Dishes/DishCard.vue
@@ -1,5 +1,4 @@
 <script>
-<<<<<<< HEAD
 export default {
   data() {
     return {
@@ -13,50 +12,6 @@ export default {
 
 <template>
   {{ console.log(dish) }}
-=======
-
-import { store } from "../../data/store";
-import axios from "axios";
-
-export default {
-    data() {
-        return{
-            dish: [],
-        }
-
-    },
-
-    methods: {
-    fetchDish(endpoint) {
-      axios
-        .get(endpoint)
-        .then((response) => {
-        this.dish = response.data;
-        console.log(this.dish);
-      });
-    },
-  },
-
-  mounted() {
-    const dishId = this.$route.params.id;
-    this.fetchDish(store.baseUri + "restaurant/" + this.id + "/menu/" + dishId);
-  },
-
-  props: ["id"],
- }
-
-
-</script>
-
-<template>
-  
-     <!-- <img :src="dish.image" :alt="dish.name">
-    <div class="details">
-      <h2>{{ dish.name }}</h2>
-      <p>{{ dish.price}} €</p>
-      <p>{{ dish.description }}</p>
-    </div> -->
->>>>>>> 546d17d28afc8d3bb13486ebee722a6cd5cac2bd
 </template>
 
 <style lang="scss" scoped></style>
