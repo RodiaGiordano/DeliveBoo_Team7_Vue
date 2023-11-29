38febdf2782a88e0c8d8dbf8e4aa9dc24dcb4fe7
plastikSun <marcom2k01@gmail.com>
2023-11-29 16:58:16 +0100

    create: navbar + router-link to cart

---
+++ src/assets/components/partials/Navbar.vue
@@ -0,0 +1,23 @@
+<script>
+export default {
+  data() {
+    return {};
+  },
+};
+</script>
+
+<template>
+  <nav class="navbar bg-body-tertiary">
+    <div class="container-fluid">
+      <span class="navbar-brand mb-0 h1">DeliveBoo</span>
+
+      <router-link :to="{ name: 'cart' }">
+        <button type="button" class="btn btn-primary">
+          Carrello
+        </button></router-link
+      >
+    </div>
+  </nav>
+</template>
+
+<style lang="scss" scoped></style>
