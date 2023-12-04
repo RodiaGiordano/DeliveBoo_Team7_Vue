ee6c224737965af9a0a6be9080dde360ad66cb7c
HiranyaRanawaka99 <hiranya.ranawaka99@gmail.com>
2023-12-04 12:26:54 +0200

    add mobile-version menulist

--- src/assets/components/Dishes/MenuList.vue
+++ src/assets/components/Dishes/MenuList.vue
@@ -14,12 +14,10 @@ export default {
 <template>
   <!-- jumbotron -->
   <div class="menulist">
-    <div class="restaurant-jumbotron">
-      <img src="../../images/jumbo-menuList1.jpg" class="restaurant-jumbo" />
-    </div>
+    <div class="restaurant-jumbotron-mobile"></div>
 
     <!-- r image -->
-    <div class="restaurant-details">
+    <div class="restaurant-details rounded-top">
       <div class="restaurant-image">
         <img :src="restaurant.image || '../../images/color-no-bg.png'" alt="" class="img-fluid" />
       </div>
@@ -29,8 +27,8 @@ export default {
         <span v-for="typeEl in restaurant.types" :key="typeEl.id">
           <small class="text-body-secondary"> &bull; {{ typeEl.name }}</small>
         </span>
-        <p><font-awesome-icon icon="fa-solid fa-map-pin"></font-awesome-icon> {{ restaurant.address }}</p>
-        <p>Descrizione: {{ restaurant.description }}</p>
+        <p class="address"><font-awesome-icon class="pin" icon="fa-solid fa-map-pin"></font-awesome-icon> {{ restaurant.address }}</p>
+        <p class="des"><span> Su di noi: </span> <br />{{ restaurant.description }}</p>
       </div>
     </div>
 
@@ -48,36 +46,83 @@ export default {
 
 <style lang="scss" scoped>
 @use '../../scss/partials/variables' as *;
+
+// .restaurant-jumbotron-full {
+//   display: none;
+// }
+
 .menulist {
-  width: 100vw;
+  width: 100%;
 
-  .restaurant-jumbotron {
-    position: relative;
+  .restaurant-jumbotron-mobile {
+    background-image: url('../../images/jumbo-menuList2.jpg');
+    height: 150px;
+    background-attachment: fixed;
+    background-position: center;
+    background-repeat: no-repeat;
+    background-size: cover;
+  }
 
-    .restaurant-jumbo {
-      width: 100%;
-      max-width: 100%;
-      height: auto;
-      object-fit: cover;
+  .restaurant-info {
+    //non funzia
+    border-top-right-radius: 100px;
+    border-top-left-radius: 100px;
+    text-align: center;
+
+    padding: 1.8rem;
+
+    .des {
+      padding-top: 0.8rem;
+
+      span {
+        font-weight: 700;
+        color: $primary-color;
+      }
+    }
+    .address {
+      padding-top: 0.8rem;
+
+      .pin {
+        color: $primary-color;
+      }
     }
   }
-
-  .restaurant-details {
-    position: absolute;
-    top: 80%;
-    left: 50%;
-    transform: translate(-50%, -50%);
-    background-color: $bg-color;
-    border: 2px solid $bg2-color;
-    padding: 1.5rem;
-    border-radius: 25px;
-    width: 80%;
-
-    text-align: center;
-  }
-
-  .dishes {
-    padding: 6rem 1rem;
-  }
 }
+
+// @media screen and (min-width: 992px) {
+// .restaurant-jumbotron-mobile {
+//   display: none;
+// }
+
+// .restaurant-jumbotron-full {
+//   display: block;
+
+//   position: relative;
+
+//   .image {
+//     width: 100%;
+//     max-width: 100%;
+//     height: auto;
+//     object-fit: cover;
+//   }
+
+//   .restaurant-details {
+//     position: absolute;
+//     top: 50%;
+//     left: 50%;
+//     transform: translate(-50%, -50%);
+//     background-color: $bg-color;
+//     border: 2px solid $bg2-color;
+//     padding: 1.5rem;
+//     border-radius: 25px;
+//     width: 80%;
+
+//     text-align: center;
+//   }
+
+//   .dishes {
+//     margin-top: 5rem;
+//   }
+//   }
+// }
 </style>
