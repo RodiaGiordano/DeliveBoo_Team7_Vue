bb309a7ad44d0d6451c945efc2e9bef6a2f66ec9
RodiaGiordano <rodiagiordano93@gmail.com>
2023-12-04 22:38:32 +0100

    add form validation client side

--- src/pages/TestPage.vue
+++ src/pages/TestPage.vue
@@ -6,10 +6,17 @@ export default {
   data() {
     return {
       loading: false,
+      paymentForm: false,
+      dataForm: true,
+      validatedForm: {},
     };
   },
 
   methods: {
+    isValid(formData) {
+      return formData.name !== '' && formData.lastName !== '' && formData.tel.match(/^\d+$/) && formData.tel !== '' && formData.address !== '';
+    },
+
     tokenCall() {
       axios
         .get(store.baseUri + 'order/generate')
@@ -27,7 +34,13 @@ export default {
               button.addEventListener('click', function () {
                 instance.requestPaymentMethod((err, payload) => {
                   axios.post(store.baseUri + 'order/make/payment', { payment_method_nonce: payload.nonce, id: 4 }).then((response) => {
-                    console.log(response);
+                    console.log(response.data);
+
+                    if (response.data.succes) {
+                      axios.post(store.baseUri + 'order/send', { formData: this.dataForm }).then((response) => {
+                        console.log(response);
+                      });
+                    }
                   });
                 });
               });
@@ -41,11 +54,29 @@ export default {
           this.loading = false;
         });
     },
+
+    submitFormCheck() {
+      const formData = {
+        name: this.$refs.inputName.value,
+        lastName: this.$refs.inputLastName.value,
+        tel: this.$refs.inputTell.value.replace(/\s+/g, ''),
+        address: this.$refs.inputAddress.value,
+        note: this.$refs.inputNote.value,
+      };
+
+      if (this.isValid(formData)) {
+        axios.post(store.baseUri + 'order', formData).then((response) => {
+          this.validatedForm = response.data;
+          this.dataForm = false;
+          this.paymentForm = true;
+
+          this.tokenCall();
+        });
+      }
+    },
   },
 
-  mounted() {
-    this.tokenCall();
-  },
+  mounted() {},
 };
 </script>
 
@@ -56,30 +87,31 @@ export default {
       <div class="spinner-border ms-auto" aria-hidden="true"></div>
     </div>
 
-    <form class="row g-3" id="ciao">
+    <form v-if="dataForm" @submit.prevent="submitForm" class="row g-3" id="ciao">
       <div class="col-md-6">
         <label for="inputName" class="form-label">Nome</label>
-        <input type="text" class="form-control" id="inputname" required />
+        <input type="text" class="form-control" ref="inputName" required />
       </div>
       <div class="col-md-6">
         <label for="inputLastName" class="form-label">Cognome</label>
-        <input type="text" class="form-control" id="inputLastName" required />
+        <input type="text" class="form-control" ref="inputLastName" value="" required />
       </div>
       <div class="col-6">
         <label for="inputTell" class="form-label">Telefono</label>
-        <input type="text" class="form-control" id="inputTell" pattern="[0-9]{9,15}" title="porco dio" required />
+        <input type="tel" pattern="[0-9\s]+" class="form-control" ref="inputTell" required />
       </div>
       <div class="col-6">
         <label for="inputAddress" class="form-label">Indirizzo</label>
-        <input type="text" class="form-control" id="inputAddress" required />
+        <input type="text" class="form-control" ref="inputAddress" required />
       </div>
       <div class="col-md-6">
-        <label for="inputNote" class="form-label">Example textarea</label>
-        <textarea class="form-control" id="inputNote" rows="3"></textarea>
+        <label for="inputNote" class="form-label">Note</label>
+        <textarea class="form-control" ref="inputNote" rows="3"></textarea>
       </div>
+      <button type="submit" @click="submitFormCheck()">Continua con il pagamento</button>
     </form>
 
-    <div class="payment mt-2">
+    <div v-if="paymentForm" class="payment mt-2">
       <div id="dropin-container"></div>
       <button form="ciao" id="submit-button" class="button button--small button--green">Purchase</button>
     </div>
