b224a1761ed7de91421b38f4ac043ed106688e9f
HiranyaRanawaka99 <hiranya.ranawaka99@gmail.com>
2023-12-05 17:04:29 +0200

    adjusted menulist layout

--- src/assets/components/Dishes/MenuList.vue
+++ src/assets/components/Dishes/MenuList.vue
@@ -14,15 +14,15 @@ export default {
 <template>
   <!-- jumbotron -->
   <div class="menulist">
-    <!-- MOBILE -->
-    <div class="restaurant-jumbotron-mobile"></div>
+    <div class="restaurant-jumbotron"></div>
     <!-- r image -->
-    <div class="restaurant-details-mobile">
+    <div class="restaurant-details">
       <div class="restaurant-image">
         <img :src="restaurant.image || '../../images/color-no-bg.png'" alt="" class="img-fluid" />
       </div>
       <!-- r info -->
       <div class="restaurant-info">
+        <div data-v-f24f87aa class="restaurant-details"></div>
         <h2>{{ restaurant.restaurant_name }}</h2>
         <p class="mb-2">
           <span v-for="typeEl in restaurant.types" :key="typeEl.id">
@@ -36,31 +36,7 @@ export default {
         <p class="des"><span> Su di noi: </span> <br />{{ restaurant.description }}</p>
       </div>
     </div>
-
-    <!-- FULL SCREEN -->
-    <div class="restaurant-jumbotron-full">
-      <!-- r image -->
-      <div class="restaurant-details-full">
-        <div class="restaurant-image">
-          <img v-if="restaurant.image" :src="restaurant.image" alt="" class="img-fluid" />
-          <div v-else></div>
-        </div>
-        <!-- r info -->
-        <div class="restaurant-info">
-          <h2>{{ restaurant.restaurant_name }}</h2>
-          <p class="mb-2">
-            <span v-for="typeEl in restaurant.types" :key="typeEl.id">
-              <small class="text-body-secondary"> &bull; {{ typeEl.name }}</small>
-            </span>
-          </p>
-          <p class="mb-2"><span> &bull; Ordine min. 10€ &bull; Consegna 1€ </span></p>
-          <p class="address mb-1"><font-awesome-icon class="pin" icon="fa-solid fa-map-pin"></font-awesome-icon> {{ restaurant.address }}</p>
-          <p><font-awesome-icon icon="fa-regular fa-clock" class="clock"></font-awesome-icon> <span>Consegna in 20 min.</span></p>
-
-          <p class="des"><span> Su di noi: </span> <br />{{ restaurant.description }}</p>
-        </div>
-      </div>
-    </div>
+    <hr />
     <!-- Dishes -->
     <div class="dishes">
       <p>Menu</p>
@@ -78,12 +54,7 @@ export default {
 .menulist {
   width: 100%;
 
-  .restaurant-jumbotron-full {
-    display: none;
-  }
-
-  .restaurant-jumbotron-mobile {
-    display: block;
+  .restaurant-jumbotron {
     background-image: url('../../images/jumbo-menuList2.jpg');
     height: 150px;
     background-attachment: fixed;
@@ -92,18 +63,22 @@ export default {
     background-size: cover;
   }
 
-  .restaurant-details-mobile {
-    position: relative;
+  .restaurant-details {
     border: 1px solid $bg-color;
+    position: relative;
     .restaurant-image {
-      width: 100px;
-      height: 100px;
+      width: 120px;
+      border-radius: 15px;
+      overflow: hidden;
+      border: 1px solid $bg-color;
 
       position: absolute;
+      left: 1.8rem;
       top: -10%;
-      // right: 0;
-      left: 8%;
-      margin: auto;
+    }
+
+    image {
+      width: 100%;
     }
   }
   .restaurant-info {
@@ -122,7 +97,7 @@ export default {
   }
 
   .dishes {
-    padding: 1rem 1.8rem 1rem;
+    padding: 1rem 1.8rem;
 
     p {
       color: $primary-color;
@@ -134,54 +109,13 @@ export default {
 }
 
 @media screen and (min-width: 768px) {
+  .restaurant-details {
+    .restaurant-image {
+      width: 140px !important;
+    }
+  }
 }
 
 @media screen and (min-width: 992px) {
-  .restaurant-jumbotron-mobile {
-    display: none !important;
-  }
-  .restaurant-details-mobile {
-    display: none !important;
-  }
-
-  .restaurant-jumbotron-full {
-    display: block !important;
-    background-image: url('../../images/jumbo-menuList1.jpg');
-    height: 500px;
-    background-position: center;
-    background-repeat: no-repeat;
-    background-size: cover;
-    .restaurant-image {
-      width: 200px !important;
-      height: 150px !important;
-    }
-    .restaurant-details-full {
-      position: relative;
-
-      background-color: $bg-color;
-      border: 1px solid $bg2-color;
-      border-radius: 50px;
-      width: 65%;
-
-      position: absolute;
-      left: 0;
-      right: 0;
-      bottom: -30%;
-      margin: auto;
-      .restaurant-info {
-        padding: 8rem 1.8rem 1rem;
-      }
-      .restaurant-image {
-        position: absolute;
-        left: 0;
-        right: 0;
-        top: -10%;
-        margin: auto;
-      }
-    }
-  }
-  .dishes {
-    padding-top: 9rem !important;
-  }
 }
 </style>
