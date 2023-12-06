8690bf279b805c67b8339a0b94278f4b16f3d57b
RodiaGiordano <rodiagiordano93@gmail.com>
2023-12-06 13:42:47 +0100

    Revert "prova 2"
    This reverts commit 756512be798ee416024456e54327157b0c24e558.

--- src/pages/HomePage.vue
+++ src/pages/HomePage.vue
@@ -28,7 +28,6 @@ export default {
   },
   // dd
 
-  // dddd
   methods: {
     fetchRestaurants(endpoint = store.baseUri + 'restaurant/') {
       axios.get(endpoint, { params: this.paramsFactory }).then((response) => {
