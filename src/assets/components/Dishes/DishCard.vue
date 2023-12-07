f64f65bc6bc4d1902496c286bfb073dd13c6b223
plastikSun <marcom2k01@gmail.com>
2023-12-07 14:36:40 +0100

    remove: debug log

--- src/assets/components/Dishes/DishCard.vue
+++ src/assets/components/Dishes/DishCard.vue
@@ -54,6 +54,7 @@ export default {
 
       const cartString = JSON.stringify(currentCart);
       localStorage.setItem('orderedDishIds', cartString);
+      console.log(this.cartStorage);
     },
   },
 
